c0467219a46d09d1599c3862d89abb6c7d9bba6e
bastian <b.marinkovic@uc.cl>
2024-06-23 14:30:02 -0400

    feat: add image upload

--- src/pages/api/series/index.ts
+++ src/pages/api/series/index.ts
@@ -3,15 +3,46 @@ import { supabase } from "../../../lib/supabase";
 
 export const POST: APIRoute = async ({ request, redirect }) => {
   const formData = await request.formData();
+
+  const imgFile = formData.get("imageUrl");
+  const title = formData.get("title");
+  const description = formData.get("description");
+
+  const baseUrl = import.meta.env.SUPABASE_URL;
+  const storageUrl = baseUrl + import.meta.env.SUPABASE_STORAGE;
+  
+  let imgUrl = null;
+
+  if (imgFile instanceof File && imgFile.size > 0) {
+    const file = imgFile as File;
+    const arrayBuffer = await file.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+    
+    imgUrl = title + Date.now().toString() + ".png";
+
+    const { data, error } = await supabase.storage
+      .from("images")
+      .upload(imgUrl, buffer, {
+        contentType: "image/png",
+      });
+
+      imgUrl = storageUrl + imgUrl;
+
+      if (error) {
+        imgUrl = null;
+      }
+    }
+
+
   const { data, error } = await supabase.from("series").insert([
     {
-      title: formData.get("title"),
+      title: title,
     //   streamingService: formData.get("streamingService"),
     //   seasons: Number(formData.get("seasons")),
     //   episodesPerSeason: Number(formData.get("episodesPerSeason")),
-      description: formData.get("description"),
+      description: description,
     //   category: formData.get("category"),
-      imageUrl: formData.get("imageUrl"),
+      imageUrl: imgUrl,
     },
   ]);
   console.log(data);
